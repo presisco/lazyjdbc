43516118216108dfa245c26da82941443426bb05
presisco <internight@sina.com>
2018-06-02 19:18:11 +0800

    add more exception checks to MapJdbcClient

--- src/main/kotlin/com/presisco/lazyjdbc/client/MapJdbcClient.kt
+++ src/main/kotlin/com/presisco/lazyjdbc/client/MapJdbcClient.kt
@@ -2,6 +2,8 @@ package com.presisco.lazyjdbc.client
 
 import com.presisco.lazyjdbc.convertion.SimpleSql2JavaConvertion
 import com.presisco.lazyjdbc.convertion.SqlTypedJava2SqlConvertion
+import java.sql.SQLException
+import java.sql.Statement
 import javax.sql.DataSource
 
 class MapJdbcClient(
@@ -16,78 +18,102 @@ class MapJdbcClient(
     private val sql2Java = SimpleSql2JavaConvertion()
     private val sqlTypeCache = HashMap<String, HashMap<String, Int>>()
 
-    fun getColumnTypeMap(tableName: String): HashMap<String, Int>{
-        if(sqlTypeCache.containsKey(tableName)){
+    fun getColumnTypeMap(tableName: String): HashMap<String, Int> {
+        if (sqlTypeCache.containsKey(tableName)) {
             return sqlTypeCache[tableName]!!
         }
 
         val columnTypeMap = HashMap<String, Int>()
         sqlTypeCache[tableName] = columnTypeMap
+
         val connection = getConnection()
         val statement = connection.createStatement()
-        statement.fetchSize = 1
-        val resultSet = statement.executeQuery("select * from  $wrapper$tableName$wrapper")
-        with(resultSet.metaData) {
-            for (i in 1..columnCount) {
-                columnTypeMap[getColumnName(i)] = getColumnType(i)
+        try {
+            statement.fetchSize = 1
+            val resultSet = statement.executeQuery("select * from  $wrapper$tableName$wrapper")
+            with(resultSet.metaData) {
+                for (i in 1..columnCount) {
+                    columnTypeMap[getColumnName(i)] = getColumnType(i)
+                }
             }
+        } catch (e: SQLException) {
+            throw RuntimeException("failed to read column types", e)
+        } finally {
+            statement.close()
+            closeConnection(connection)
         }
-        statement.close()
-        closeConnection(connection)
         return columnTypeMap
     }
 
-    fun insert(tableName: String, mapList: List<Map<String, Any?>>){
-        if(mapList.isEmpty())
-            return
+    fun insert(tableName: String, mapList: List<Map<String, Any?>>): Set<Int> {
+        val failedSet = HashSet<Int>()
+
+        if (mapList.isEmpty())
+            return failedSet
+
         val columnTypeMap = getColumnTypeMap(tableName)
 
         val columnList = mapList[0].keys.toList()
         val sqlTypeArray = Array(columnList.size, { 0 })
-        columnList.forEachIndexed { index , column -> sqlTypeArray[index] = columnTypeMap[column]!! }
+        columnList.forEachIndexed { index, column -> sqlTypeArray[index] = columnTypeMap[column]!! }
 
         val connection = getConnection()
         val statement = connection.prepareStatement(buildInsertSql(tableName, columnList))
-        val buffer = Array<Any?>(columnList.size,{ null })
+        val buffer = Array<Any?>(columnList.size, { null })
         val java2sql = SqlTypedJava2SqlConvertion(sqlTypeArray)
 
-        mapList.forEach{
-            map ->
+        try {
+            mapList.forEach { map ->
 
-            columnList.forEachIndexed{ index, column -> buffer[index] = map[column] }
-            java2sql.bindArray(buffer, statement)
+                columnList.forEachIndexed { index, column -> buffer[index] = map[column] }
+                java2sql.bindArray(buffer, statement)
 
-            statement.addBatch()
+                statement.addBatch()
+            }
+
+            val resultArray = statement.executeBatch()
+            if (resultArray.contains(Statement.EXECUTE_FAILED)) {
+                if (rollbackOnBatchFailure) {
+                    connection.rollback()
+                }
+                resultArray.forEachIndexed { i, result -> if (result == Statement.EXECUTE_FAILED) failedSet.add(i) }
+            }
+            connection.commit()
+        } catch (e: SQLException) {
+            throw RuntimeException("failed to insert maps", e)
+        } finally {
+            statement.close()
+            connection.close()
         }
-
-        statement.executeBatch()
-        connection.commit()
-        statement.close()
-        connection.close()
+        return failedSet
     }
 
-    fun select(sql: String): List<Map<String, Any?>>{
+    fun select(sql: String): List<Map<String, Any?>> {
+        val resultList = ArrayList<Map<String, Any?>>()
         val connection = getConnection()
         val statement = connection.createStatement()
-        val resultSet = statement.executeQuery(sql)
-        val resultList = ArrayList<Map<String, Any?>>()
-        val metadata = resultSet.metaData
-        val columnNameArray = Array(metadata.columnCount, { "" })
-        with(resultSet.metaData){
-            for(i in 1..columnCount){
-                columnNameArray[i - 1] = metadata.getColumnName(i)
+        try {
+            val resultSet = statement.executeQuery(sql)
+            val metadata = resultSet.metaData
+            val columnNameArray = Array(metadata.columnCount, { "" })
+            with(resultSet.metaData) {
+                for (i in 1..columnCount) {
+                    columnNameArray[i - 1] = metadata.getColumnName(i)
+                }
             }
-        }
-        while(resultSet.next()){
-            val map = HashMap<String, Any?>()
-            val row = sql2Java.toArray(resultSet)
-            columnNameArray.forEachIndexed { index, name -> map[name] = row[index] }
+            while (resultSet.next()) {
+                val map = HashMap<String, Any?>()
+                val row = sql2Java.toArray(resultSet)
+                columnNameArray.forEachIndexed { index, name -> map[name] = row[index] }
 
-            resultList.add(map)
+                resultList.add(map)
+            }
+        } catch (e: SQLException) {
+            throw RuntimeException("failed to execute sql: $sql", e)
+        } finally {
+            statement.close()
+            connection.close()
         }
-        resultSet.close()
-        statement.close()
-        connection.close()
         return resultList
     }
 
