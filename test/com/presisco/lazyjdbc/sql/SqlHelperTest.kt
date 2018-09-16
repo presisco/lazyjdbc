9f4a2b1a42a43759078c0a4a26f6eb40a983ba2c
presisco <internight@sina.com>
2018-09-16 15:56:16 +0800

    add more tests

--- test/com/presisco/lazyjdbc/sql/SqlHelperTest.kt
+++ test/com/presisco/lazyjdbc/sql/SqlHelperTest.kt
@@ -7,13 +7,13 @@ class SqlHelperTest {
     @Test
     fun validate() {
         val tableName = "example"
-        val colums = arrayOf("name", "class", "score")
+        val columns = arrayOf("name", "class", "score")
         val rows = arrayListOf(
                 arrayOf("james", "1", 100),
                 arrayOf("bob", "2", 20),
                 arrayOf("tom", null, 80)
         )
-        println(SqlHelper.buildBatchInsertString(tableName, colums, rows))
+        println(SqlHelper.buildBatchInsertString(tableName, columns, rows))
     }
 
 }
